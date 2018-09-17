2b72295e5776b16b62d4114ac5bcbc0b683259e3
Charlotte Hagborg <charlotte.hagborg@infinera.com>
2018-09-17 21:56:27 +0200

    Added ifndef + file header.

--- src/lib/component/component.h
+++ src/lib/component/component.h
@@ -1,2 +1,14 @@
+/**
+ * @file component.h
+ * @brief Header file for component choices
+ *
+ * @author Charlotte Hagborg
+ * @date 17 Sep 2018
+ */
+
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
 int e_resistance(float orig_resistance,
                  float* res_array);
+#endif //COMPONENT_H
