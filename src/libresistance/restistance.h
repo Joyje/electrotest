0eb99c351c90cf8f3c4f766294687aa5f74afef1
Joel Sahlin <Joel.I.Sahlin@gmail.com>
2018-09-17 19:43:17 +0200

    Skapade libresistance och main

---
+++ src/libresistance/restistance.h
@@ -0,0 +1,6 @@
+#ifndef RESISTANCE
+#define RESISTANCE
+
+float calc_resistance(int count, char conn, float *array);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+int main (void) {
+  return (RETVALUE);
+}
