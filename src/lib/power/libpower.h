f5bf5b81c94dafb0984608b6ce19ec0883fc1a4c
Lars Bergvall <lars.m.bergvall@gmail.com>
2018-09-18 17:18:18 +0200

    added libpower library

---
+++ src/lib/power/libpower.h
@@ -0,0 +1,7 @@
+#ifndef POWER_H
+#define POWER_H
+
+float calc_power_r(float volt, float resistance);
+float calc_power_i(float volt, float current);
+
+#endif
---
+++ src/lib/power/powerTest.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// Ska räkna ut P = U * I (spänning genom ström) - Hur gör jag ohm till ampere?
+// Volt = Spänning
+// Resistance = ohm
+// Ström = ampere, A
+#include <math.h>
+#include "libpower.h"
+
+int main(int argc, char *argv[]) {
+	printf("Running test for libpower\n");
+
+	printf("50 volts and 1398 ohm (resistance) should give us 1.78w in return.\n");
+	float power_r = calc_power_r(50.0, 1398);
+	float power_i = calc_power_i(50.0, 0.0358);
+
+	printf("power_r: %.2f \n", power_r);
+	printf("power_i: %.2f \n", power_i);
+	
+	return 0;
+}
