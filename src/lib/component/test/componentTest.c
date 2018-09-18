4ba7575f1ccb41c517cc455b28941e17de53ae16
Charlotte Hagborg <charlotte.hagborg@infinera.com>
2018-09-18 18:17:59 +0200

    Try to use lib from main.

--- src/lib/component/test/componentTest.c
+++ src/lib/component/test/componentTest.c
@@ -1,8 +1,12 @@
 
 #include <stdio.h>
+#include "../component.h"
 
 int main(int argc, char *argv[])
 {
+    int result = 0;
     printf( "Hello World\n");
+    result = e_resistance(5.0, NULL);
+    printf("%d", result);
     return 0;
 }
