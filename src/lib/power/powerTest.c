bd07a8d468758e95c85cfbd30cbe6b552e1104ad
Lars Bergvall <lars.m.bergvall@gmail.com>
2018-09-19 14:10:49 +0200

    Added some comments to powerTest

--- src/lib/power/powerTest.c
+++ src/lib/power/powerTest.c
@@ -4,7 +4,12 @@
 int main(int argc, char *argv[]) {
 	printf("***** Running test for libpower *****\n\n");
 
+	// Using the values provided in the assignment description for this test
+	// (50volt and 1398 ohm)
 	float power_r = calc_power_r(50.0, 1398);
+
+	// For this function, we take the current (ampere) as input.
+	// Current = volt/resistance = 50/1398 = 0.0358
 	float power_i = calc_power_i(50.0, 0.0358);
 
 	printf("50 volts and 1398 ohm (resistance) should give us 1.79w in return.\n");
