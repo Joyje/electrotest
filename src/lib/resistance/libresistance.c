7f790f306fa8f853da8eed8c8c558b038ff6b843
Joel Sahlin <Joel.I.Sahlin@gmail.com>
2018-09-19 06:52:55 +0200

    Uppdaterade libresistance

--- src/lib/resistance/libresistance.c
+++ src/lib/resistance/libresistance.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
-#include "resistance.h"
 
 float calc_resistance(int count, char conn, float *array){
-	return 0;
+	float resistance = 0;
+	if (count > 0 ){
+		if (conn == 'P') {
+			fprintf(stdout, "Du har angett parallellkoppling\n");
+			for (int i = 0; i < count; i++) {
+				resistance += 1/array[i];
+			}
+			if (resistance > 0){
+				resistance = 1/resistance;
+			}
+			return resistance;
+
+		} else if (conn == 'S'){
+			fprintf(stdout, "Du har angett seriekoppling\n");
+			for (int i = 0; i < count; i++){
+				resistance += array[i];
+			}
+			return resistance;
+		} else {
+			fprintf(stderr, "Ogiltig koppling. Enbart värdena S (seriell) och P (parallell) godtas\n");
+			return -1;
+		}
+	} else {
+		fprintf(stderr, "Inga resistorer. Ange minst en resistor.\n");
+		return -1;
+	}
 }
