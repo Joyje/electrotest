18449ef47d510a78250a4fea033537ef691744e8
Lars Bergvall <lars.m.bergvall@gmail.com>
2018-09-19 13:48:01 +0200

    corrected 1.78 => 1.79

--- src/lib/power/powerTest.c
+++ src/lib/power/powerTest.c
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[]) {
 	printf("Running test for libpower\n");
 
-	printf("50 volts and 1398 ohm (resistance) should give us 1.78w in return.\n");
+	printf("50 volts and 1398 ohm (resistance) should give us 1.79w in return.\n");
 	float power_r = calc_power_r(50.0, 1398);
 	float power_i = calc_power_i(50.0, 0.0358);
 
