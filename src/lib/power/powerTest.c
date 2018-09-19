db7ec1b93c27044b80dd975e096f01678d3b52f0
Lars Bergvall <lars.m.bergvall@gmail.com>
2018-09-19 14:07:03 +0200

    Small changes to powerTest; it's output is now more specific about what it does

--- src/lib/power/powerTest.c
+++ src/lib/power/powerTest.c
@@ -2,13 +2,14 @@
 #include "libpower.h"
 
 int main(int argc, char *argv[]) {
-	printf("Running test for libpower\n");
+	printf("***** Running test for libpower *****\n\n");
 
-	printf("50 volts and 1398 ohm (resistance) should give us 1.79w in return.\n");
 	float power_r = calc_power_r(50.0, 1398);
 	float power_i = calc_power_i(50.0, 0.0358);
 
-	printf("power_r: %.2f \n", power_r);
+	printf("50 volts and 1398 ohm (resistance) should give us 1.79w in return.\n");
+	printf("power_r: %.2f \n\n", power_r);
+	printf("50 volts and 0.0358A should give us 1.79w in return\n");
 	printf("power_i: %.2f \n", power_i);
 	
 	return 0;
