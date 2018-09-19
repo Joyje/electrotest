83dddb43fa2ed9d74bd97f72c88d810e1a2dd583
Joel Sahlin <Joel.I.Sahlin@gmail.com>
2018-09-19 08:38:19 +0200

    Förbättrade libresistance.

--- src/lib/resistance/libresistance.c
+++ src/lib/resistance/libresistance.c
@@ -2,29 +2,31 @@
 
 float calc_resistance(int count, char conn, float *array){
 	float resistance = 0;
-	if (count > 0 ){
-		if (conn == 'P') {
-			fprintf(stdout, "Du har angett parallellkoppling\n");
-			for (int i = 0; i < count; i++) {
-				resistance += 1/array[i];
-			}
-			if (resistance > 0){
-				resistance = 1/resistance;
-			}
-			return resistance;
-
-		} else if (conn == 'S'){
-			fprintf(stdout, "Du har angett seriekoppling\n");
-			for (int i = 0; i < count; i++){
-				resistance += array[i];
-			}
-			return resistance;
-		} else {
-			fprintf(stderr, "Ogiltig koppling. Enbart värdena S (seriell) och P (parallell) godtas\n");
-			return -1;
-		}
-	} else {
+	if (count < 1 ){
 		fprintf(stderr, "Inga resistorer. Ange minst en resistor.\n");
+		return -1;
+	} else if (conn == 'P') {
+		fprintf(stdout, "Du har angett parallellkoppling\n");
+		for (int i = 0; i < count; i++) {
+			if (array[i] < 0) {
+				fprintf(stderr, "Ogiltigt värde på komponent %d. %f\n", i+1, array[i]);
+				return -1;
+			}
+			resistance += 1/array[i];
+		}
+		return 1/resistance;
+	} else if (conn == 'S'){
+		fprintf(stdout, "Du har angett seriekoppling\n");
+		for (int i = 0; i < count; i++){
+			if (array[i] < 0) {
+				fprintf(stderr, "Ogiltigt värde på komponent %d. %f\n", i+1, array[i]);
+				return -1;
+			}
+			resistance += array[i];
+		}
+		return resistance;
+	} else {
+		fprintf(stderr, "Ogiltig koppling. Enbart värdena S (seriell) och P (parallell) godtas\n");
 		return -1;
 	}
 }
