9ec6331986ca7b5bcdafbcde08a545dd4dd14d40
Joel Sahlin <Joel.I.Sahlin@gmail.com>
2018-09-21 04:38:40 +0200

    Snyggade till koden i electrotest.c

--- src/electrotest.c
+++ src/electrotest.c
@@ -8,32 +8,23 @@
 
 int main(void) {
 
-	// First, we need to get the volt:
-	fprintf(stdout, "Ange spänningskälla i volt:");
-
-	char voltInput[8];
-	fgets (voltInput, sizeof (voltInput), stdin);
-	float volt = atof(voltInput);
-
-	// Then we need to get the connection:
+	//First we create a variable that temorarily stores the users input
 	char i[8];
 
-	char conn;
+	// Then, we need to get the volt:
+	fprintf(stdout, "Ange spänningskälla i volt:");
+	fgets (i, sizeof (i), stdin);
+	float volt = atof(i);
+
+	// Then we need to get the connection:
 	fprintf(stdout, "Ange koppling [S | P]:");
-	conn = toupper(getchar());
+	char conn = toupper(getchar());
 	getchar();
 
-	// We need the connection value to be in correct format; if we don't get it we may as well terminate
-	if (conn != 'S' && conn != 'P') {
-		fprintf(stderr, "Fel input, du måste ange S eller P!\n");
-		return -1;
-	}
-
 	// ... number of components:
-	int count;
 	fprintf(stdout, "Antal komponenter:");
 	fgets (i, sizeof (i), stdin);
-	count = atoi(i);
+	int count = atoi(i);
 
 	// and finally, the ohm values of each component
 	float array[count];
@@ -45,8 +36,7 @@ int main(void) {
 
 
 	// Calculate the resistance:
-	float ohm = 0;
-	ohm = calc_resistance(count, conn, array);
+	float ohm = calc_resistance(count, conn, array);
 	printf("Ersättningsresistans:\n%.1f ohm.\n", ohm);
 
 	// Get the effect using the resistance:
